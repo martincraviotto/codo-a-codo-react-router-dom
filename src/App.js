91b80c339fe87465d5240cd4b3163816c554c0a3
Martin <martincraviotto@gmail.com>
2023-10-18 12:09:33 -0300

    routeo por una página inexistente

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Contactos } from './componentes/Contactos';
+import { Error404 } from './componentes/Error404';
 import { Home } from './componentes/Home';
 import { Routes, Route } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="contactos" element={<Contactos />}></Route>
+        <Route path="*" element={<Error404 />}></Route>
       </Routes>
 
       
---
+++ src/componentes/Error404.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Error404 = () => {
+  return (
+    <>
+        <div>Error404</div>
+        <h2>Estás intentando ingresar en una pagina no existente</h2>
+    </>
+  )
+}
